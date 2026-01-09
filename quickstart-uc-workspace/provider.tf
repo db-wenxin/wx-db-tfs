68c4150f788ad4c842552c8ca97ce9626e1d9b70
wenxin-liu_data <wenxin.liu@databricks.com>
2026-01-09 15:04:49 -0500

    fmt

--- quickstart-uc-workspace/provider.tf
+++ quickstart-uc-workspace/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     databricks = {
       source  = "databricks/databricks"
-      version = ">=1.10.0"
+      version = ">=1.100.0"
     }
     aws = {
       source  = "hashicorp/aws"
